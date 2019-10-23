52b942ea211fe0c577833039a273bf7afc8ecde5
Akōchi <me@akochi.eu>
2019-10-23 18:52:48 +0200

    Very basic hug module

---
+++ Commands/HugModule.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace derpy.Commands
+{
+    public class HugModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("hug")]
+        public async Task Hug()
+        {
+            var guildUser = Context.Guild.GetUser(Context.User.Id);
+            await ReplyAsync($"_hugs {guildUser.Nickname}_");
+        }
+    }
+}
