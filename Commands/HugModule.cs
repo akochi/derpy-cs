38944df7f58fb1108764f260e0c3bd3f2ae0a9ce
Akōchi <me@akochi.eu>
2019-10-25 20:20:36 +0200

    Hug everyone

--- Commands/HugModule.cs
+++ Commands/HugModule.cs
@@ -15,12 +15,19 @@ namespace derpy.Commands
             "_pounces on {0} for a hug_",
         };
 
+        static readonly string[] GROUP_HUGS = {
+            "Woah, that's a lot of people to hug at once!",
+        };
+
         [Command]
         [Alias("me")]
         public async Task Hug() => await HugOne(Context.Guild.GetUser(Context.User.Id));
 
         [Command]
         public async Task Hug(SocketUser user) => await HugOne(Context.Guild.GetUser(user.Id));
+
+        [Command("everyone")]
+        public async Task HugEveryone() => await ReplyAsync(GROUP_HUGS.PickRandom());
 
         private async Task HugOne(IGuildUser user) => await ReplyAsync(string.Format(HUGS.PickRandom(), user.Name()));
     }
