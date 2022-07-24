ec133e1fc64a3d2554a3330ae2a840f81710bd78
Akōchi <me@akochi.eu>
2022-07-24 20:46:25 +0200

    NixOS module

--- flake.nix
+++ flake.nix
@@ -21,5 +21,21 @@
     in rec {
         packages.x86_64-darwin.default = derpy { pkgs = import nixpkgs { system = "x86_64-darwin"; }; };
         packages.x86_64-linux.default = derpy { pkgs = import nixpkgs { system = "x86_64-linux"; }; };
+
+        nixosModules.default = { pkgs }: let
+            derpyPkg = derpy { inherit pkgs; };
+        in {
+            systemd.services.derpy = {
+                enable = true;
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                    ExecStart = "${derpyPkg}/bin/Derpy";
+                    RestartSec = 5;
+                    Restart = "always";
+                    WorkingDirectory = "/var/run/Derpy";
+                    DynamicUser = true;
+                };
+            };
+        };
     };
 }
